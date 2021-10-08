bff3a89a14bb156d998066de0c79f5e1eac236d2
IndraRusyana <81315984+IndraRusyana@users.noreply.github.com>
2021-10-08 20:32:08 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReactDOM, { Component } from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './components/App';
 // tempelkan code yang disiapkan dikolom instruksi dibawah
 ReactDOM.render(App, document.getElementById('root'));
